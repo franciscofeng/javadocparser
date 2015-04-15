788bbcbd6bb7d07f53ed9a64f6438433cb608ded
franciscofeng <franciscofeng@gmail.com>
2015-04-15 22:17:28 +0800

    add filter out function to skip java.applet java.awt javax.swing and org package

--- JavaDocParser/src/main/java/com/feng/demo/JavaDocParser/App.java
+++ JavaDocParser/src/main/java/com/feng/demo/JavaDocParser/App.java
@@ -13,40 +13,66 @@ import org.jsoup.select.Elements;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+	public static String[] USELESS_PACKAGE =
+	{ "java.applet", "java.awt", "javax.swing", "org" };
+
 	public List<String> getAllLinks(String rootURL)
 	{
 		List<String> urls = new ArrayList<String>();
-        Document rootDoc = null;
-		try {
+		Document rootDoc = null;
+		try
+		{
 			rootDoc = Jsoup.connect(rootURL).get();
-		} catch (IOException e1) {
+		} catch (IOException e1)
+		{
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-        Elements links = rootDoc.select("a[href]");
-        String baseURL = this.getBaseURL(rootURL);
-        
-        for(Element e:links)
-        {
-        	urls.add(baseURL+e.attr("href"));
-        }
+		Elements links = rootDoc.select("a[href]");
+		String baseURL = this.getBaseURL(rootURL);
+
+		for (Element e : links)
+		{
+			if(isAvailableURL(e.attr("href")))
+			{
+				urls.add(baseURL + e.attr("href"));
+			}
+		}
 		return urls;
 	}
+
 	private String getBaseURL(String rootURL)
 	{
-		return rootURL.substring(0, rootURL.lastIndexOf("/")+1);
+		return rootURL.substring(0, rootURL.lastIndexOf("/") + 1);
 	}
-    public static void main( String[] args )
-    {
-        String rootURL = "http://docs.oracle.com/javase/8/docs/api/allclasses-noframe.html";
-        App app = new App();
-        List<String> urls = app.getAllLinks(rootURL);
-        for(String url:urls)
-        {
-        	System.out.println(url);
-        }
 
-    }
+	private boolean isAvailableURL(String url)
+	{
+		String fullName = getFullName(url);
+		for(String useless:USELESS_PACKAGE)
+		{
+			if(fullName.startsWith(useless))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	private String getFullName(String url)
+	{
+		return url.replaceAll("/", ".");
+	}
+	public static void main(String[] args)
+	{
+		String rootURL = "http://docs.oracle.com/javase/8/docs/api/allclasses-noframe.html";
+		App app = new App();
+		List<String> urls = app.getAllLinks(rootURL);
+		for (String url : urls)
+		{
+			System.out.println(url);
+		}
+
+	}
 }
